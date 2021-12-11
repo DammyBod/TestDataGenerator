fa833a94f47b1445bc6ed93b0cea628ba8818a76
DammyBod <59138291+DammyBod@users.noreply.github.com>
2021-12-11 16:18:48 +0100

    Update RandomDetails.py

--- FileDataGenerator/Generator/RandomDetails.py
+++ FileDataGenerator/Generator/RandomDetails.py
@@ -8,5 +8,8 @@ def get_color_name():
 def get_random_digit():
     return obj.random_digit()
 
+def get_null_value():
+    return obj.null_value()
+
 def get_date():
     return obj.date(pattern="%Y-%m-%d", end_datetime=None)
